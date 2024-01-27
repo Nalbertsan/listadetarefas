3606d92259bad6a6a462fdffcce2d10fd35a9c35
Nalbertsan <nalbert1000@gmail.com>
2024-01-27 01:22:33 -0300

    :+1: Update para correção Varcel

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,9 +1,9 @@
 export type taskType = {
   id?: number,
-  title?: string,
-  description?: string,
-  status?: 'pendente' | 'executando' | 'concluida',
-  date?: string,
+  title: string,
+  description: string,
+  status: 'pendente' | 'executando' | 'concluida',
+  date: string,
 }
 
 export type selectOptions = {
